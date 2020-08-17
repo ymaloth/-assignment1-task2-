580764b59373a16fa16b12b3ad50bda18a066578
ymaloth <yakuswamy123@gmail.com>
2020-08-18 01:31:30 +1000

    button create

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+let app2 = express()
+
+ app.use(express.static('public'))
+
+/*app.get('/', function (req, res) {
+  res.send('Instagram clone')
+})
+app.get('/',function(request,response){
+    response.send('this is insta clone')
+
+})*/
+
+
+ 
+// start the server on port 3000
+app.listen(3000)
